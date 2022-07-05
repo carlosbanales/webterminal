b306619bd4b4ecd274a87cf6f84b8753048f1499
carlosbanales <charliebananas42@gmail.com>
2022-07-04 17:48:16 -0700

    added todo list

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,13 @@ let prompt = document.getElementById('prompt');
 let conatainer = document.getElementById('container');
 let down = 0;
 
+// Checklist
+// - save input data to variable
+// - display saved data on previous line when the enter key is pressed
+// - allow up and down key to step though saved inputs
+// - create commands
+// - add a header and a set of instructions
+
 function moveDown() {
 	down+=40;
 	prompt.style.top = down + 'px';
